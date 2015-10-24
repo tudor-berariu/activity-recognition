dedc3dea78dca209e5647adeb2f5fb1eeaba3d14
Tudor Berariu <tudor.berariu@gmail.com>
2015-10-24 20:17:09 +0300

    Added script to extract archives

---
+++ data/extract.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RAW_FOLDER=./raw/
+
+for f in `ls archives/*.tar.gz`; do
+    echo -n "Extracting $f..."
+    #tar -zxf $f -C $RAW_FOLDER
+    echo "... done!" 
+done
+
+for f in `ls archives/*.zip`; do
+    echo -n "Extracting $f..."
+    #unzip -q -d $RAW_FOLDER $f
+    echo "... done!"
+done
