13d7b61a277f97e4b20baae53ba96a10f1cfd78e
Mihai Trăscău <mihai.trascau@gmail.com>
2015-10-24 20:26:17 +0300

    Added mkdir in extract.sh

--- data/extract.sh
+++ data/extract.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 RAW_FOLDER=./raw/
+mkdir -p RAW_FOLDER
 
 for f in `ls archives/*.tar.gz`; do
     echo -n "Extracting $f..."
-    #tar -zxf $f -C $RAW_FOLDER
+    tar -zxf $f -C $RAW_FOLDER
     echo "... done!" 
 done
 
 for f in `ls archives/*.zip`; do
     echo -n "Extracting $f..."
-    #unzip -q -d $RAW_FOLDER $f
+    unzip -q -d $RAW_FOLDER $f
     echo "... done!"
 done
