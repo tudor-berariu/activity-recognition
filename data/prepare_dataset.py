bd0e3dadac3efcf3d47a0cd3c1e5c14f5e3aa517
Tudor Berariu <tudor.berariu@gmail.com>
2015-11-07 14:39:54 +0200

    Started script that prepares the datasets

---
+++ data/prepare_dataset.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def prepare_static_dataset():
+    pass
+
+def prepare_dynamic_dataset():
+    pass
+
+
+if __name__ == "__main__":
+    print("Preparing data sets...")
+    prepare_static_dataset()
+    prepare_dynamic_dataset()
+    print("Done!")
