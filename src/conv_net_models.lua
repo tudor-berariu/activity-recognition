f0f1053eb3cec6594eee0d13ffea51c9b22425db
Tudor Berariu <tudor.berariu@gmail.com>
2015-12-06 22:42:16 +0200

    Added a conv net; added one epoch of training

---
+++ src/conv_net_models.lua
@@ -0,0 +1,56 @@
+local module conv_net_models = {}
+require("nn")
+
+function conv_net_models.get_network(args)
+   local nn = require("nn")
+   local args = args or {}
+   local width = args.width or 110
+   local height = args.height or 110
+   local classes_no = args.classes_no or 4
+   local model_name = args.model or "5x5conv"
+   local model
+   if model_name == "5x5conv" then
+      model = nn.Sequential()
+      --- first convolution
+      model:add(nn.SpatialZeroPadding(2, 2, 2, 2))
+      model:add(nn.SpatialConvolutionMap(nn.tables.full(1, 32), 5, 5, 1, 1))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+      width = math.ceil(width / 2)
+      height = math.ceil(height / 2)
+      print("anticipated (1): " .. height .. "x" .. width)
+      --- second convolution
+      model:add(nn.SpatialZeroPadding(2, 2, 2, 2))
+      model:add(nn.SpatialConvolutionMap(nn.tables.full(32, 64), 5, 5, 1, 1))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+      width = math.ceil(width / 2)
+      height = math.ceil(height / 2)
+      print("anticipated (2): " .. height .. "x" .. width)
+      --- third convolution
+      model:add(nn.SpatialZeroPadding(1, 1, 1, 1))
+      model:add(nn.SpatialConvolutionMap(nn.tables.full(64, 128), 3, 3, 1, 1))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+      width = math.ceil(width / 2)
+      height = math.ceil(height / 2)
+      print("anticipated (3): " .. height .. "x" .. width)
+      --- fourth convolution
+      model:add(nn.SpatialZeroPadding(1, 1, 1, 1))
+      model:add(nn.SpatialConvolutionMap(nn.tables.full(128, 128), 3, 3, 1, 1))
+      model:add(nn.ReLU())
+      model:add(nn.SpatialMaxPooling(2, 2, 2, 2):ceil())
+      width = math.ceil(width / 2)
+      height = math.ceil(height / 2)
+      print("anticipated (4): " .. height .. "x" .. width)
+      --- a last fully connected layer
+      model:add(nn.Reshape(128 * height * width))
+      model:add(nn.Linear(128 * height * width, classes_no))
+      model:add(nn.Tanh())
+   end
+
+   model:add(nn.LogSoftMax())
+   return model
+end
+
+return conv_net_models
--- src/train_conv_net.lua
+++ src/train_conv_net.lua
@@ -1,21 +1,46 @@
-ds = require('activity_dataset')
-require('torch')
+local ds = require("activity_dataset")
+local conv_models = require("conv_net_models")
 
-function get_mini_batch(img_names, labels, B)
-   local N = #img_names
-   local idx = torch.rand(B):mul(N):ceil():long()
-   local batch_names = {}
-   for i = 1, B do batch_names[i] = img_names[idx[i]] end
-   local X = ds.get_posture_batch(batch_names)
-   local T = labels:index(1, idx)
-   return X, T
+function sgd_epoch(model, images, labels, classes, args)
+   local criterion = nn.ClassNLLCriterion()
+   local parameters, gradParameters = model:getParameters()
+   local confusion = optim.ConfusionMatrix(classes)
+   args = args or {}
+   local batch_size = args.batch_size or 20
+   local idx = torch.randperm(images:size(1))
+   -- go through the dataset
+   local iter = 0
+   for i = 1, images:size(1), batch_size do
+      xlua.progress(t, images:size(1) + 1)
+      j = math.min(images:size(1), i + batch_size -1)
+
+      local feval = function(x)
+         if x ~= parameters then
+            parameters:copy(x)
+         end
+         gradParameters:zero()
+         local f = 0
+         local input = images:index(1, idx[{{i, j}}])
+         local targets = labels:index(1, idx[{{i, j}}]
+         local output = model:forward()
+         local df_do = torch.Tensor(output:size(1), labels:size(2))
+         for k = 1, output:size(1) do
+            local err = criterion:forward(output[k], targets[k])
+            f = f + err
+            df_do[k]:copy(criterion:backward(output[k], targets[k]))
+            confusion:add(output[k], targets[k])
+         end
+         model:backward(inputs, df_do)
+         gradParameters:div(j - i + 1)
+         f = f / (j - i + 1)
+         return f, gradParameters
+      end
+      optim.sgd(feval, parameters, args)
+      iter = iter + 1
+      if iter % 1000 == 0 then
+         collectgarbage("collect")
+      end
+   end
+   xlua.progress(images:size(1), images:size(1)+1)
+   return confusion
 end
-
-do
-   local img_names, class_names, labels = ds.get_posture_dataset()
-   local N = #img_names
-   local X, T
-   X, T = get_mini_batch(img_names, labels, 20)
-   print(X:size())
-   print(T:size())
-end -- do
