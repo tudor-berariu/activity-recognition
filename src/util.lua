6082a18694885068086ce924b05661eb411d9ef1
Tudor Berariu <tudor.berariu@gmail.com>
2015-12-07 13:16:27 +0200

    Added deep eq for tables

--- src/util.lua
+++ src/util.lua
@@ -7,7 +7,7 @@ local module util = {}
 --]]
 
 function util.reverse_table(t)
-   u = {}
+   local u = {}
    for k, v in pairs(t) do u[v] = k end
    return u
 end
@@ -24,6 +24,24 @@ function util.shallow_eq(t1, t2)
    end
    for k2, v2 in pairs(t2) do
       if not t1[k2] or t1[k2] ~= v2 then return false end
+   end
+   return true
+end
+
+--[[
+
+   Deep comparison of tables.
+
+--]]
+
+function util.eq(t1, t2, depth)
+   if type(t1) ~= type(t2) then return false end
+   if type(t1) ~= "table" then return t1 == t2 end
+   for k1, v1 in pairs(t1) do
+      if not t2[k1] or not util.eq(t2[k1], v1) then return false end
+   end
+   for k2, v2 in pairs(t2) do
+      if not t1[k2] or not util.eq(t1[k2], v2) then return false end
    end
    return true
 end
