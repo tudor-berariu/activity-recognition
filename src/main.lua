9b131af8ce6840f4d5ca6b22841e7eecf9dc0936
Tudor Berariu <tudor.berariu@gmail.com>
2016-02-04 22:59:17 +0200

    Revised main script; using CmdLine now

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,48 @@
-require("torch")
-require("train_conv_net")
+--------------------------------------------------------------------------------
+--- 1. Load required libraries
 
-do
-   args = {
-      -- general arguments
-      ["verbose"] = true,
-      ["plot"] = true,
-      ["visualize"] = true,
-      -- dataset
-      ["scale"] = 0.2,
-      ["limit"] = 1000,
-      ["one_of_k"] = true,
-      ["filter"] = {["sitting"] = true, ["standing"] = true},
-      -- preprocess
-      ["crop_height"] = 91,
-      ["crop_width"] = 91,
-      -- training
-      ["momentum"] = 0.9,
-      ["learningRate"] = 1.0
-   }
-   train_model(args)
-end
+require("torch")
+-- require("train_conv_net")
+
+--------------------------------------------------------------------------------
+--- 2. Parse command line options
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text("Activity recognition!")
+cmd:text()
+cmd:text("Options:")
+
+-- General : the most important
+cmd:option("-dataset", "posture", "the dataset to solve (posture / activity)")
+cmd:option("-model", "logistic", "the model to be used for classification")
+
+-- Info during training
+cmd:option("-verbose", true, "display information")
+cmd:option("-visualize", true, "display images during training")
+cmd:option("-plot", true, "display plot during training")
+
+-- Hardware
+cmd:option("-gpuid", -1, "GPU id; -1 for CPU")
+
+-- Optimization paramters
+cmd:option("-learning_rate", 1e-3, "learning rate")
+cmd:option("-momentum", .0, "momentum")
+cmd:option("-batch_size", 10, "batch size")
+cmd:option("-max_epochs", 100, "number of maximum epochs")
+
+-- Miscellaneous
+cmd:option("-seed", 666, "seed for the random number generator")
+
+-- Parse
+opt = cmd:parse(arg)
+
+--------------------------------------------------------------------------------
+--- 3. Train the model
+
+torch.manualSeed(opt.seed)
+
+--------------------------------------------------------------------------------
+--- 4. Done!
+
+print("Done!")
