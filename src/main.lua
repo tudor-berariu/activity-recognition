d6da6429f70550cad0199eb49578f8ac00a16b0d
Tudor Berariu <tudor.berariu@gmail.com>
2016-02-09 18:34:23 +0200

    Tested dataset loader and added various options

--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,9 @@
 --- 1. Load required libraries
 
 require("torch")
--- require("train_conv_net")
+
+require("activity_dataset")
+--train = require("train")
 
 --------------------------------------------------------------------------------
 --- 2. Parse command line options
@@ -13,34 +15,81 @@ cmd:text("Activity recognition!")
 cmd:text()
 cmd:text("Options:")
 
--- General : the most important
+--- General : the most important
+
 cmd:option("-dataset", "posture", "the dataset to solve (posture / activity)")
 cmd:option("-model", "logistic", "the model to be used for classification")
+cmd:option("-see_data", false, "just see the data")
 
--- Info during training
-cmd:option("-verbose", true, "display information")
-cmd:option("-visualize", true, "display images during training")
-cmd:option("-plot", true, "display plot during training")
+--- Dataset
 
--- Hardware
+cmd:option("-no_cache", false, "use tensors cached on disk (if they exist)")
+cmd:option("-limit", 0, "include only the first n images in the dataset")
+cmd:option("-one_output", false, "encode targets as single values; def: 1-of-k")
+cmd:option("-just_names", false, "do not load images, but only their names")
+cmd:option("-test_ratio", 0.2, "the ratio used for testing")
+cmd:option("-shuffle", false, "shuffle all before train / valid / test split")
+
+--- Dataset augmentation
+
+cmd:option("-no_flip", false, "horizontal flip")
+cmd:option("-vert_crop_ratio", 0.1, "vertical crop ratio")
+cmd:option("-horiz_crop_ratio", 0.2, "horizontal crop ratio")
+cmd:option("-vert_crop", 0, "vertical crop ratio")
+cmd:option("-horiz_crop", 0, "horizontal crop ratio")
+
+--- Info during training
+
+cmd:option("-verbose", false, "display information")
+cmd:option("-visualize", false, "display images during training")
+cmd:option("-plot", false, "display plot during training")
+
+--- Hardware
+
 cmd:option("-gpuid", -1, "GPU id; -1 for CPU")
 
--- Optimization paramters
+--- Optimization paramters
+
+cmd:option("-batch_size", 10, "batch size")
+cmd:option("-not_contiguous", false, "do not take contiguous batches")
+
 cmd:option("-learning_rate", 1e-3, "learning rate")
 cmd:option("-momentum", .0, "momentum")
-cmd:option("-batch_size", 10, "batch size")
 cmd:option("-max_epochs", 100, "number of maximum epochs")
 
--- Miscellaneous
+--- Miscellaneous
+
 cmd:option("-seed", 666, "seed for the random number generator")
 
--- Parse
+--- Parse
+
 opt = cmd:parse(arg)
+
+opt.use_cache = not opt.no_cache
+opt.flip = not opt.no_flip
+opt.contiguous = not opt.not_contiguous
+opt.one_of_k = not opt.one_output
+
+torch.manualSeed(opt.seed)
 
 --------------------------------------------------------------------------------
 --- 3. Train the model
 
-torch.manualSeed(opt.seed)
+ds = ActivityDataset(opt)
+
+if opt.see_data then
+   require("image")
+
+   repeat
+      local X, T = ds:update_batch()
+      local band = X[1]:clone()
+      for i=2,ds.batch_size do
+         band = torch.cat(band, X[i])
+      end
+      win = image.display{image=band, win=win, zoom=0.5, legend='batch'}
+   until ds.epoch_finished
+end -- if opt.see_data
+
 
 --------------------------------------------------------------------------------
 --- 4. Done!
